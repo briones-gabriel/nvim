a05b7b5fe63e9ba6f54c97bda0fa5281155be3e2
Briones Dos Santos Gabriel Dario <dossantosgabrielb@gmail.com>
2021-05-05 00:18:00 -0300

    Added packer nvim.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,16 +2,16 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+  fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
+  execute "packadd packer.nvim"
 end
 
 -- Plugins list
---[[return require("packer").packer.startup(
-	function()
-		use "gruvbox-community/gruvbox"
-	end
-)]]
+return require("packer").startup(
+    function()
+        use "wbthomason/packer.nvim"
+    end
+)
