dfb65b7f56a8c5ca8e661506e653d2bd763fa7d7
Briones Dos Santos Gabriel Dario <dossantosgabrielb@gmail.com>
2021-05-04 23:48:00 -0300

    Started porting init.vim to init.lua

---
+++ lua/keybindings.lua
@@ -0,0 +1,46 @@
+--[[ nmap <Leader>t :NERDTreeFind<CR>
+
+vmap <Leader>/ <Plug>NERDCommenterToggle
+nmap <Leader>/ <Plug>NERDCommenterToggle
+
+nmap <silent> gd <Plug>(coc-definition)
+nmap <silent> gi <Plug>(coc-implementation)
+nmap <silent> gr <Plug>(coc-references)
+nmap <leader>rn <Plug>(coc-rename)
+
+nnoremap <C-p> :GFiles<CR>
+
+nnoremap <Leader>m :MaximizerToggle<CR>
+vnoremap <Leader>m :MaximizerToggle<CR>
+
+-- navigate chunks of current buffer
+nmap [g <Plug>(coc-git-prevchunk)
+nmap ]g <Plug>(coc-git-nextchunk)
+
+-- navigate conflicts of current buffer
+nmap [c <Plug>(coc-git-prevconflict)
+nmap ]c <Plug>(coc-git-nextconflict)
+
+-- show chunk diff at current position
+nmap gs <Plug>(coc-git-chunkinfo)
+
+-- show commit contains current position
+nmap gc <Plug>(coc-git-commit)
+]]
+
+local options = { noremap = true, silent = true }
+
+-- Common keybindings
+vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", options)
+vim.api.nvim_set_keymap("n", "<Leader>q", ":q<CR>", options)
+
+-- Buffer control
+vim.api.nvim_set_keymap("n", "<Leader>+", ":vertical resize +15<CR>", options)
+vim.api.nvim_set_keymap("n", "<Leader>-", ":vertical resize -15<CR>", options)
+vim.api.nvim_set_keymap("n", "<Leader>=", "<C-w>=", options)
+
+-- Buffer navigation
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-W><C-H>", options)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-W><C-J>", options)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-W><C-K>", options)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-W><C-L>", options)
---
+++ lua/settings.lua
@@ -0,0 +1,33 @@
+-- Global
+vim.g.termguicolors = true
+vim.g.showmatch = true
+vim.g.hlsearch = false
+vim.g.showmode = false
+vim.g.hidden = true
+vim.g.errorbells = false
+vim.g.laststatus = 2
+vim.g.clipboard = "unnamedplus"
+vim.g.splitbelow = true
+vim.g.splitright = true
+
+-- Window
+vim.wo.relativenumber = true
+vim.wo.cursorline = true
+vim.wo.number = true
+vim.wo.wrap = false
+vim.wo.scrolloff = 4
+vim.wo.colorcolumn = "120"
+
+-- Buffer
+vim.o.tabstop = 4
+vim.bo.tabstop = 4
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
+vim.o.expandtab = true
+
+-- Leader
+vim.g.mapleader = " "
+vim.b.mapleader = " "
+
+-- Don't know how to
+vim.cmd("highlight ColorColumn ctermbg=0 guibg=none")
