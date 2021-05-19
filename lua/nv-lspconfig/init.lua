a74975542a3ca76affbfda7145285e3f246c61ea
Briones Dos Santos Gabriel Dario <dossantosgabrielb@gmail.com>
2021-05-19 12:13:48 -0300

    Added some lsp keybindings

--- lua/nv-lspconfig/init.lua
+++ lua/nv-lspconfig/init.lua
@@ -1,14 +1,4 @@
 -- Configuration
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-        'documentation',
-        'detail',
-        'additionalTextEdits',
-    }
-}
-
 require('vim.lsp.protocol').CompletionItemKind = {
       '  Text';          -- = 1
       '  Function';      -- = 2;
@@ -43,4 +33,5 @@ vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", Key
 vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", Keymap_options)
 vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", Keymap_options)
 vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", Keymap_options)
-
+vim.api.nvim_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", Keymap_options)
+vim.api.nvim_set_keymap("n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", Keymap_options)
